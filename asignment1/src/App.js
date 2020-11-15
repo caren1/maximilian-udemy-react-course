77c2241025eb8e9aba213ab28a575bb4aee36cff
caren1 <czarnocki.w@wp.pl>
2020-11-15 18:43:57 +0100

    3

--- asignment1/src/App.js
+++ asignment1/src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import UserOutput from './UserOutput/UserOutput'
 
 function App() {
   return (
     <div className="App">
-
+      <UserOutput />
+      <UserOutput />
+      <UserOutput />
+      <UserOutput />
     </div>
   );
 }
--- asignment1/src/UserOutput/UserOutput.js
+++ asignment1/src/UserOutput/UserOutput.js
@@ -3,8 +3,8 @@ import React from 'react'
 const UserOutput = () => {
     return (
         <>
-            <p></p>
-            <p></p>
+            <p>Hello there!</p>
+            <p>User output here</p>
         </>
     )
 }
