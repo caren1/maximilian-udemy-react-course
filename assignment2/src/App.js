7ffef8e5c94746056f9c4c5169b02893c7e3ba6b
caren1 <czarnocki.w@wp.pl>
2020-11-16 13:25:25 +0100

    a2 - 1

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,11 +1,26 @@
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  state = {
+    inputText: ''
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ 
+      inputText: event.target.value
+     })
+  }
+
+  render(){
   return (
     <div className="App">
-
+      <input type="text" onChange={this.handleInputChange} />
+      <p>{this.state.inputText.length}</p>
     </div>
   );
+}
 }
 
 export default App;
