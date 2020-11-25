2e5aed5578fed4e79a144d7ee55265cbae7e3efb
caren1 <czarnocki.w@wp.pl>
2020-11-25 18:07:29 +0100

    initial axios request, modifying and transforming the received data

--- http-ajax-exercise/src/components/Post/Post.js
+++ http-ajax-exercise/src/components/Post/Post.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './Post.css';
 
-const post = (props) => (
+const post = ({ post }) => (
     <article className="Post">
-        <h1>Title</h1>
+        <h1>{post.title}</h1>
         <div className="Info">
-            <div className="Author">Author</div>
+            <div className="Author">{post.author}</div>
         </div>
     </article>
 );
--- http-ajax-exercise/src/containers/Blog/Blog.js
+++ http-ajax-exercise/src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,27 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: [],
+    }
+
+
+    componentDidMount = () => {
+        const posts = axios.get(`https://jsonplaceholder.typicode.com/posts`)
+        .then(response => {
+            const posts = response.data.slice(0, 4);
+            const updatedPosts = posts.map(post => ({ ...post, author: 'Max' }))
+            this.setState({ posts: updatedPosts })
+        })
+    }
+
     render () {
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {this.state.posts.map((post) => (
+                        <Post key={post.id} post={post}/>
+                    ))}
                 </section>
                 <section>
                     <FullPost />
