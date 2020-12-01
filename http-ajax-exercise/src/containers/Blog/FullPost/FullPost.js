d9243e251c89120d6f7cf8270827a179878cfbbe
caren1 <czarnocki.w@wp.pl>
2020-12-01 14:24:35 +0100

    creating dynamic nested routes - lifcycle fixes

--- http-ajax-exercise/src/containers/Blog/FullPost/FullPost.js
+++ http-ajax-exercise/src/containers/Blog/FullPost/FullPost.js
@@ -11,8 +11,17 @@ class FullPost extends Component {
 
     // we'll be sending an http request if we have an id
     componentDidMount = () => {
+       this.loadData();
+    }
+
+    componentDidUpdate = () => {
+        this.loadData();
+    }
+
+    loadData = () => {
+        console.log(this.props);
         if (this.props.match.params.id) {
-            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+            if (!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== Number(this.props.match.params.id))) {
                 axios.get(`/posts/${this.props.match.params.id}`)
                 .then(response => {
                     this.setState({ loadedPost : response.data })
@@ -22,7 +31,7 @@ class FullPost extends Component {
     }
 
     handlePostDelete = () => {
-        axios.delete(`/posts/${this.props.id}`)
+        axios.delete(`/posts/${this.props.match.params.id}`)
         .then(response => {
             console.log(response);
         })
@@ -30,7 +39,7 @@ class FullPost extends Component {
 
     render () {
         let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>
-        if (this.props.id) post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>
+        if (this.props.match.params.id) post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>
         if (this.state.loadedPost){
             post = (
                 <div className="FullPost">
