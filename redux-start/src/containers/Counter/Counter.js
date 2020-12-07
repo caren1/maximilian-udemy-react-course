bf20d5e3f44988b0d2b8edfc04d5fa4be41a694a
caren1 <czarnocki.w@wp.pl>
2020-12-07 20:10:27 +0100

    connecting store to react

--- redux-start/src/containers/Counter/Counter.js
+++ redux-start/src/containers/Counter/Counter.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+// it is a function that returns Higher Order Component and needs to be wrapped our component export
+import { connect } from 'react-redux';
+
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
@@ -28,7 +31,7 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
+                <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
@@ -38,4 +41,18 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+// we pass two pieces of info to connect
+// 1- which part of the whole application state is interesting to us
+// 2- which actions I want to dispatch
+
+// state managed in redux, is not the same as we did in class components
+// state is something that we change internally from components
+// we'll be getting the state from props now
+const mapStateToProps = state => {
+    return {
+        // give me the state value of counter that is managed by redux
+        ctr: state.counter
+    };
+};
+
+export default connect(mapStateToProps)(Counter);
--- redux-start/src/index.js
+++ redux-start/src/index.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
-import reducer from './store/reducer'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+
+// we typically initialize the store in the main file of our application
+import { createStore } from "redux";
+
+// while reducer is being set up in the separate file as it contains a lot of logic in more complex apps
+import reducer from "./store/reducer";
+
+// react-redux allows us to hook up the redux with react
+// where Provider is a helper component, we then wrap our app with it
+// it allows to kind of inject our store into the react components
+// for hooking it up, we set up a property 'store', with it the store is connected
+import { Provider } from "react-redux";
 
 const store = createStore(reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
