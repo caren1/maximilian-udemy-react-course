076f5c5319c9cbd1eb640088c2583d1dc4cc8fab
caren1 <czarnocki.w@wp.pl>
2020-12-08 20:40:27 +0100

    combined reducers

--- redux-start/src/containers/Counter/Counter.js
+++ redux-start/src/containers/Counter/Counter.js
@@ -1,32 +1,48 @@
-import React, { Component } from 'react';
-import * as actionTypes from '../../store/actions'
+import React, { Component } from "react";
+import * as actionTypes from "../../store/actions";
 
 // it is a function that returns Higher Order Component and needs to be wrapped our component export
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import CounterControl from '../../components/CounterControl/CounterControl';
-import CounterOutput from '../../components/CounterOutput/CounterOutput';
+import CounterControl from "../../components/CounterControl/CounterControl";
+import CounterOutput from "../../components/CounterOutput/CounterOutput";
 
 class Counter extends Component {
-
-    render () {
-        return (
-            <div>
-                <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
-                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
-                <CounterControl label="Add 5" clicked={() => this.props.onAddCounter(5)}  />
-                <CounterControl label="Subtract 5" clicked={() => this.props.onSubtractCounter(5)}  />
-                <hr />
-                <button onClick={this.props.onStoreResult}>Store Result</button>
-                <ul>
-                    {this.props.storedResults.map(result => (
-                        <li key={result.id} onClick={() => this.props.onDeleteResult(result.id)}>{result.value}</li>
-                    ))}
-                </ul>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <CounterOutput value={this.props.ctr} />
+        <CounterControl
+          label="Increment"
+          clicked={this.props.onIncrementCounter}
+        />
+        <CounterControl
+          label="Decrement"
+          clicked={this.props.onDecrementCounter}
+        />
+        <CounterControl
+          label="Add 5"
+          clicked={() => this.props.onAddCounter(5)}
+        />
+        <CounterControl
+          label="Subtract 5"
+          clicked={() => this.props.onSubtractCounter(5)}
+        />
+        <hr />
+        <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
+        <ul>
+          {this.props.storedResults.map((result) => (
+            <li
+              key={result.id}
+              onClick={() => this.props.onDeleteResult(result.id)}
+            >
+              {result.value}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 // we pass two pieces of info to connect
@@ -36,24 +52,26 @@ class Counter extends Component {
 // state managed in redux, is not the same as we did in class components
 // state is something that we change internally from components
 // we'll be getting the state from props now
-const mapStateToProps = state => {
-    return {
-        // give me the state value of counter that is managed by redux
-        ctr: state.counter,
-        storedResults: state.results
-    };
+const mapStateToProps = (state) => {
+  return {
+    // give me the state value of counter that is managed by redux
+    ctr: state.ctr.counter,
+    storedResults: state.res.results,
+  };
 };
 
-const mapDispatchToProps = dispatch => {
-    // it will call store.dispatch behind the scenes
-    return {
-        onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
-        onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
-        onAddCounter: (number) => dispatch({ type: actionTypes.ADD, value: number }),
-        onSubtractCounter: (number) => dispatch({ type: actionTypes.SUBTRACT, value: number }),
-        onStoreResult: () => dispatch({ type: actionTypes.STORE_RESULT }),
-        onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, id }),
-    }
+const mapDispatchToProps = (dispatch) => {
+  // it will call store.dispatch behind the scenes
+  return {
+    onIncrementCounter: () => dispatch({ type: actionTypes.INCREMENT }),
+    onDecrementCounter: () => dispatch({ type: actionTypes.DECREMENT }),
+    onAddCounter: (number) =>
+      dispatch({ type: actionTypes.ADD, value: number }),
+    onSubtractCounter: (number) =>
+      dispatch({ type: actionTypes.SUBTRACT, value: number }),
+    onStoreResult: (result) => dispatch({ type: actionTypes.STORE_RESULT, result }),
+    onDeleteResult: (id) => dispatch({ type: actionTypes.DELETE_RESULT, id }),
+  };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
--- redux-start/src/index.js
+++ redux-start/src/index.js
@@ -5,16 +5,23 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
 // we typically initialize the store in the main file of our application
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 
 // while reducer is being set up in the separate file as it contains a lot of logic in more complex apps
-import reducer from "./store/reducer";
+// import reducer from "./store/reducer";
+import counterReducer from './store/reducers/counter'
+import resultReducer from './store/reducers/result'
 
 // react-redux allows us to hook up the redux with react
 // where Provider is a helper component, we then wrap our app with it
 // it allows to kind of inject our store into the react components
 // for hooking it up, we set up a property 'store', with it the store is connected
 import { Provider } from "react-redux";
+
+const reducer = combineReducers({
+  ctr: counterReducer,
+  res: resultReducer
+});
 
 const store = createStore(reducer);
 
