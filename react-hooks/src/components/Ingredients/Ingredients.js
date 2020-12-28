e3f052563e7a9e0e9d3764a4a3a7695b137bdf66
caren1 <czarnocki.w@wp.pl>
2020-12-28 15:52:49 +0100

    useEffect and loading data

--- react-hooks/src/components/Ingredients/Ingredients.js
+++ react-hooks/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -7,23 +7,40 @@ import Search from "./Search";
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
 
-  const addIngredient = (ingredient) => {
-
-    fetch('https://react-hooks-b227a-default-rtdb.firebaseio.com/ingredients.json', {
-      method: 'POST',
-      body: JSON.stringify(ingredient),
-      headers: { 'Content-Type':'application/json' }
-    })
-    .then(response => {
-      return response.json();
+  useEffect(() => {
+    fetch(
+      "https://react-hooks-b227a-default-rtdb.firebaseio.com/ingredients.json"
+    ).then((response) => {
+      return response.json()
     })
     .then(responseData => {
-      const newIngredients = ingredients.concat({
-        id: responseData.name,
-        ...ingredient,
-      });
-      setIngredients(newIngredients);
+      const loadedIngredients = []
+      for (let key in responseData) {
+        loadedIngredients.push({ id:key, title: responseData[key].title, amount: responseData[key].amount })
+      }
+      setIngredients(loadedIngredients);
     })
+  }, []);
+
+  const addIngredient = (ingredient) => {
+    fetch(
+      "https://react-hooks-b227a-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(ingredient),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        const newIngredients = ingredients.concat({
+          id: responseData.name,
+          ...ingredient,
+        });
+        setIngredients(newIngredients);
+      });
   };
 
   const removeIngredient = (id) => {
@@ -39,7 +56,10 @@ const Ingredients = () => {
 
       <section>
         <Search />
-        <IngredientList ingredients={ingredients} onRemoveItem={removeIngredient} />
+        <IngredientList
+          ingredients={ingredients}
+          onRemoveItem={removeIngredient}
+        />
       </section>
     </div>
   );
